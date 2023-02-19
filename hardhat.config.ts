edf8e7d6b602d18b40640bc7ac7c23d0f62afcf0
Bruno Fialho <brugerf@gmail.com>
2023-02-19 19:43:56 -0300

    feat: mint on goerli and opensea testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `${process.env.STAGING_QUICKNODE_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContractFactory = await ethers.getContractFactory('MyEpicNFT');
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
   let txn = await nftContract.makeAnEpicNFT()
   await txn.wait()
+  console.log("Minted NFT #1")
 
   txn = await nftContract.makeAnEpicNFT()
   await txn.wait()
+  console.log("Minted NFT #2")
 };
 
 async function runMain() {
